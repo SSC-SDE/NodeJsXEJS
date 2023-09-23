4da4c9e29d03ac31338ad9814376a0e8a8cfc518
SSC-SDE <sobaiamakechene@gmail.com>
2023-09-24 00:19:27 +0530

    updating progress

--- contact_list/index.js
+++ contact_list/index.js
@@ -1,18 +1,69 @@
 const express = require('express');
+const { readdir } = require('fs');
 const path = require('path');
 const port = 8000;
 
 const app = express();
 
-
 app.set('view engine','ejs');
 app.set('views', path.join(__dirname, 'views'));
+app.use(express.urlencoded());
+app.use(express.static('assets'));
+
+//middleware custom
+// app.use(function(req, res, next){
+//     req.myName="Shankhya"
+//     // console.log('middleware 1 called');
+//     next();
+// });
+
+// //middleware 2
+// app.use(function(req, res,next){
+//     console.log('My Name from MW2', req.myName);
+//     // console.log('middleware 2 called');
+//     next();
+// });
+
+var contactList = [
+    {
+        name: "Shankhya",
+        phone: "123456789"
+    },
+    {
+        name: "Tony",
+        phone: "12222222"
+    },
+    {
+        name: "John Snow",
+        phone: "2441139"
+    }
+
+]
+
 
 
 app.get('/', function(req,res){
-    
-    return res.render('home', {title:"I am flying"});
+    //console.log('from the get route controller',req.myName);
+    return res.render('home', {
+
+        title:"contact_list",
+        contact_list: contactList
+    });
 });
+
+app.post('/create-contact', function(req,res){
+//    contactList.push({
+//         name: req.body.name,
+//         phone: req.body.phone
+//    });
+
+   contactList.push(req.body);
+
+   return res.redirect('back');
+
+});
+
+
 
 app.get('/practice', function(req, res){
     return res.render('practice', {
